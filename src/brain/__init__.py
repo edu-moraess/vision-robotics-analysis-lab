5551b175dc04c47f22e749dc60a38b4410eac342
Eduardo Moraes <199427299+edu-moraess@users.noreply.github.com>
2026-08-22 15:03:28 -0300

    feat(brain): export UncertaintyEngine

--- src/brain/__init__.py
+++ src/brain/__init__.py
@@ -1,9 +1,12 @@
 from .risk_engine import RiskEngine, RiskAssessment
 from .decision_engine import DecisionEngine, SceneDecision
+from .uncertainty import UncertaintyEngine, UncertaintyReport
 
 __all__ = [
     "RiskEngine",
     "RiskAssessment",
     "DecisionEngine",
     "SceneDecision",
+    "UncertaintyEngine",
+    "UncertaintyReport",
 ]
